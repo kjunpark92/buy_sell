d39c4a39414cc75122afde37078cc24b51d58467
Jun <kjunpark92@gmail.com>
2018-12-19 17:53:54 +0900

    editted header

--- html/header.php
+++ html/header.php
@@ -2,8 +2,8 @@
 session_start();
 include('db.php');
 if(isset($_SESSION['username'])){
-$username = $_SESSION['username'];
-$req = $db->prepare('SELECT id, username FROM users WHERE username = :username');
+    $username = $_SESSION['username'];
+    $req = $db->prepare('SELECT id, username FROM users WHERE username = :username');
         $req->execute(array(
         'username' => $username));
         $result = $req->fetch();
@@ -17,12 +17,14 @@ $logout = "";
 $registration = "";
 if(isset($isIndexPage) AND $isIndexPage) {
     $url = "./html/in_category.php?cat=";
+    $index = "index.php";
     $profile = "./html/profile.php";
     $login = "./html/log_in.php";
     $logout = "./html/log_out.php";
     $registration = "./html/registration.php";
 }else {
     $url = "in_category.php?cat=";
+    $index = "../index.php";
     $profile = 'profile.php';
     $login = "log_in.php";
     $logout = "log_out.php";
@@ -33,7 +35,7 @@ if(isset($isIndexPage) AND $isIndexPage) {
 <header>
     <div id="navbar">
         <div id="top_logo">
-            <a href="../index.php"> logo <img src="" alt="" ></a>
+            <a href="<?php echo $index;?>"> logo <img src="" alt="" ></a>
         </div>
         <div class="category">
             <span><a href="<?php echo $url;?>electronics">Electronics</a></span>
