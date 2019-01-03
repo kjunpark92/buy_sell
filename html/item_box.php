233703698d986ec91e74d465562480034899e346
Jun <kjunpark92@gmail.com>
2019-01-03 13:45:15 +0900

    fixed item box in while loop

--- html/item_box.php
+++ html/item_box.php
@@ -1,13 +1,6 @@
-<?php
-include('db.php');
-$req = $db->prepare('SELECT * FROM posts');
-$req->execute(array());
-$result = $req -> fetchall();
-?>
-    
 <div class = 'item_wrapper'>
     <div class='item_img'>
     <img src=<?php echo $img ?>/> <span class="title_price"><?php echo $category ?></span><br></div>
-    <span class='title_price'><?php echo $title . " " . $price ?></span><br>
+    <span class='title_price'><?php echo $title . " " ; echo "$".$price ?></span><br>
     <span class='district_town'><?php echo $district."-".$town?></span>
 </div>
